1e0defd5cf0a4c317e3aa9f398b6121ba2b7d078
Ayush Parihar <ayushparihar@Ayushs-MacBook-Pro.local>
2023-03-15 19:16:17 +0530

    Internationalization/jaxb/concurrent collection/ExecutorService

---
+++ src/jaxb/JaxbMain.java
@@ -0,0 +1,20 @@
+package jaxb;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class JaxbMain {
+    public static void main(String[] args) throws JAXBException {
+        Customer customer=new Customer();
+        customer.setCustomerName("Ayush");
+        customer.setCustomerId(123);
+        customer.setAddress("Mac");
+
+        JAXBContext context = JAXBContext.newInstance(Customer.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.marshal(customer,System.out);
+
+
+    }
+}
